00b6c199cfc00f22d79b160e08f2852dd4e40039
Zzawadii <hopevzawadii@gmail.com>
2026-01-08 08:42:45 +0200

    Revise service worker caching strategy
    Updated cached files and added icon files.

--- service-worker.js
+++ service-worker.js
@@ -1,39 +1,38 @@
 // Name of the cache
 const CACHE_NAME = 'koffee-cache-v1';
 
-// Files to cache for offline use
+// Files to cache
 const urlsToCache = [
-  './',
-  'index.html',
-  'about.html',
-  'menu.html',
-  'contact.html',
-  'secnd.css',
-  'about.css',
-  'menu.css',
-  'contact.css',
-  'koffee.jpg',
-  'arabic.jpg',
-  'roasted.jpg',
-  'ground.jpg',
-  'pack.jpg',
-  'join.jpg'
+    '/index.html',
+    '/about.html',
+    '/menu.html',
+    '/contact.html',
+    '/secnd.css',
+    '/about.css',
+    '/menu.css',
+    '/koffee.jpg',
+    '/arabic.jpg',
+    '/roasted.jpg',
+    '/ground.jpg',
+    '/pack.jpg',
+    '/join.jpg',
+    '/icons/icon-192.png',
+    '/icons/icon-512.png'
 ];
 
 // Install event: cache files
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
-  );
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+            .then(() => console.log('Files cached successfully'))
+    );
 });
 
-// Fetch event: serve cached files when offline
+// Fetch event: serve cached files if offline
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
+    );
 });
